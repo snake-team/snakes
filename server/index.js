74fc22ccddd56a0203af2f3c32027c2466a86be1
Daniel Shu <shudaniel95@gmail.com>
2019-02-02 18:57:27 -0500

    comment

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
   // res.end();
 })
+//Hi
 
 module.exports = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
